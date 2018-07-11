b4ec238c01ccb8cd752f37b94c7c37916037fd2a
Ricardo Cunha <seczeus13@gmail.com>
2018-07-11 14:45:01 +0100

    Ruby POO

---
+++ Caneta.rb
@@ -0,0 +1,20 @@
+class Caneta
+
+	@@tapada = false
+
+	def tapar
+		@@tapada = true
+	end
+
+	def destapar
+		@@tapada = false
+	end
+
+	def escrever
+		if @@tapada
+			puts "Impossivel escrever"
+		else
+			puts "Estou a escrever."
+		end
+	end
+end
---
+++ app.rb
@@ -0,0 +1,5 @@
+require_relative "Caneta"
+
+c1 = Caneta.new
+c1.tapar
+c1.escrever
