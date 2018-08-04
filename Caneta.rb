c3c5903331bad448d86650c27b7048e9cbfcf5e2
Nuno Martins <nunoormaker21@gmail.com>
2018-08-04 19:58:11 +0100

    Adição de comentários na classe

--- Caneta.rb
+++ Caneta.rb
@@ -3,6 +3,9 @@ class Caneta
 	attr_accessor :cor
 	attr_reader :marca, :forma, :tapada
 
+	# Método construtor
+	# Aceita os parâmetros cor, marca e forma
+	# Inicializa os atributos @tapada, @cor, @marca, @forma e @carga
 	def initialize(cor, marca, forma)
 		@tapada = false
 		@cor = cor
@@ -11,14 +14,21 @@ class Caneta
 		@carga = 100.0
 	end
 
+	# Método tapar()
+	# Muda o valor do atributo @tapada para true
 	def tapar
 		@tapada = true
 	end
 
+	# Método destapar()
+	# Muda o valor do atributo @tapada para false
 	def destapar
 		@tapada = false
 	end
 
+	# Método escrever()
+	# Exibe o texto "Estou a escrever com a cor #{@cor}", no caso do atributo @tapada ser false
+	# Inclui sistema de perda de carga a cada escrita
 	def escrever
 		if @tapada
 			puts "Impossivel escrever"
@@ -28,6 +38,9 @@ class Caneta
 		end
 	end
 
+	# Método escreverFrase(txt)
+	# Exibe um texto personalizado, no caso do atributo @tapada ser false
+	# Inclui sistema de perda de carga a cada escrita
 	def escreverFrase(txt)
 		if @tapada
 			puts "Impossível escrever"
@@ -37,6 +50,8 @@ class Caneta
 		end
 	end
 
+	# Método recarregar()
+	# Recarrega a carga de volta aos 100% originais
 	def recarregar
 		if @carga < 100
 			@carga = 100
@@ -45,6 +60,8 @@ class Caneta
 		end
 	end
 
+	# Método estado()
+	# Exibe o estado do objeto recorrendo ao método inspect() da classe Object
 	def estado
 		puts self.inspect
 	end
